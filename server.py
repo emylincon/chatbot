ed51315c9b930b0f07c5ee1044d9a0790ade987c
Emeka Ugwuanyi <emylincon@gmail.com>
2019-11-10 19:16:31 +0000

    fixed the delayed display response while the voice is active by putting them in a thread

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 #from chatbot import get_response
 from rihanna import get_response
 from rihanna import rihanna_voice
+from threading import Thread
 
 
 class ChatServer(WebSocket):
@@ -9,14 +10,20 @@ class ChatServer(WebSocket):
     def handleMessage(self):
         # echo message back to client
         message = self.data
-        response = get_response(message)
-        self.sendMessage(response)
+        response = str(get_response(message))
+        # self.sendMessage(response)
+        h1 = Thread(target=self.sendMessage, args=(response,))
+        h2 = Thread(target=rihanna_voice, args=(response,))
+        h1.start()
+        h2.start()
+        '''
         if str(response)[:3] == '- -':
             rihanna_voice(str(response)[3:])
         elif str(response)[0] == '-':
             rihanna_voice(str(response)[1:])
         else:
             rihanna_voice(response)
+        '''
 
     def handleConnected(self):
         print(self.address, 'connected')
