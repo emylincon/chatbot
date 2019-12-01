a3763826dd29ba545bff759dca376c2a28682e77
Emeka Ugwuanyi <emylincon@gmail.com>
2019-12-01 19:58:52 +0000

    improved so you can choose the league

--- rihanna_football.py
+++ rihanna_football.py
@@ -19,3 +19,25 @@ def match_today():
             scores += result
 
     return scores
+
+
+def match_today_(_id):
+    competition_ids = ['Serie A', 'Primera Division', 'Eredivisie',
+                       'Primera Division', 'Ligue 1', 'Bundesliga',
+                       'Primeira Liga', 'Premier League']
+
+    req = requests.get("https://api.football-data.org/v2/matches", headers=headers)
+    data = req.json()
+    scores = ''
+    if _id in competition_ids:
+        for i in data['matches']:
+            if i['competition']['name'] == _id:
+                team1 = i['homeTeam']['name']
+                team2 = i['awayTeam']['name']
+                s_team1 = i['score']['fullTime']['homeTeam']
+                s_team2 = i['score']['fullTime']['awayTeam']
+                result = f"{team1} {s_team1} <-> {s_team2} {team2}\n"
+                scores += result
+        return scores
+    else:
+        return f"{_id} Division is not Available"
