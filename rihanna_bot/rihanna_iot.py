9d49bb4f1195429bc885e285db6054aec7590e08
Emeka Ugwuanyi <emylincon@gmail.com>
2020-01-16 12:51:26 +0000

    improved rihanna_iot

--- rihanna_bot/rihanna_iot.py
+++ rihanna_bot/rihanna_iot.py
@@ -17,6 +17,20 @@ def selector(msg):
         host_ip = msg[len("iot light on for")+1:].strip()
         send_client(host_ip, 'light on')
         return "light switched on"
+    elif msg[:len("iot temperature for")] == "iot temperature for":
+        host_ip = msg[len("iot temperature for") + 1:].strip()
+        data = send_recv_client(host_ip, "last temp")
+        return "last temp is ", data if data[0].isdigit() else data
+    elif msg[:len("iot cpu for")] == "iot cpu for":
+        host_ip = msg[len("iot cpu for") + 1:].strip()
+        data = send_recv_client(host_ip, "cpu util")
+        return "last cpu utilization is ", data if data[0].isdigit() else data
+    elif msg[:len("iot memory for")] == "iot memory for":
+        host_ip = msg[len("iot memory for") + 1:].strip()
+        data = send_recv_client(host_ip, "mem util")
+        return "last memory utilization is ", data if data[0].isdigit() else data
+    else:
+        return "Rihanna is busy at the moment"
 
 
 def fin2(barr, dfile):  #this returns a image
@@ -61,6 +75,25 @@ def send_client(host, msg):
             s.close()
     except Exception as e:
         print('Error from send_client: ', e)
+
+
+def send_recv_client(host, msg):
+
+    port = 65001  # The port used by the server
+
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
+            s.sendall(msg.encode())
+            time.sleep(1)
+            data = s.recv(1024)
+            reply = data.decode()
+            s.sendall('exit'.encode())
+            s.close()
+            return reply
+    except Exception as e:
+        print('Error from send_client: ', e)
+        return "server not responding"
 
 
 def iot_graph(query):
