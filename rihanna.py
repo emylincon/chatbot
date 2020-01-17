3c848596697198bb1d3c6420559f1d0742fbcb38
Emeka Ugwuanyi <emylincon@gmail.com>
2020-01-17 13:56:37 +0000

    added rihanna_wc to rihanna core

--- rihanna.py
+++ rihanna.py
@@ -5,7 +5,7 @@ import wikipedia
 import pyttsx3
 from selenium import webdriver
 from rihanna_bot import rihanna_football, rihanna_speak, rihanna_tweet, rihanna_news, rihanna_skype, rihanna_one_char, \
-    rihanna_time, rihanna_maths as calc, rihanna_email, rihanna_tfl, rihanna_spell, rihanna_facebook, rihanna_amazon, rihanna_dict, rihanna_iot
+    rihanna_time, rihanna_maths as calc, rihanna_email, rihanna_tfl, rihanna_spell, rihanna_facebook, rihanna_amazon, rihanna_dict, rihanna_iot, rihanna_wc
 import config
 import random as r
 
@@ -198,6 +198,9 @@ def rihanna(message):
     elif message in _time:
         reply = rihanna_time.rihanna_time()
         return reply
+
+    elif message[:len('word cloud')] == 'word cloud':
+        return rihanna_wc.selector(message)
 
     elif message[0:7] == 'what is':
         try:
