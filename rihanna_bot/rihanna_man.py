4659f7cea68f302f401b0d2415f9a4f00b9bf6b1
Emeka Ugwuanyi <emylincon@gmail.com>
2020-01-26 21:12:17 +0000

    improved the manual page

--- rihanna_bot/rihanna_man.py
+++ rihanna_bot/rihanna_man.py
@@ -40,71 +40,309 @@ def man_maths():
 
 
 def man_twitter():
-    pass
+    display = "<table id='t01'>\
+                    <tr>\
+                        <th>Twitter Usage</th>\
+                    </tr>\
+                    "
+    func = ["show global trending topics on twitter",
+            "show global trending topics on twitter graph",
+            "show trending topics on twitter",
+            "show my twitter status",
+            "show twitter status for <user_twitter_id>",
+            "show my last tweet",
+            "show last twitter status for <user_twitter_id>",
+            "tweet <message>",
+            "search twitter <search_query>"]
+    for i in func:
+        display += f"<tr>\
+                        <td>{i}</td>\
+                    </tr>"
+    say = "Find below How to use the Twitter feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_tfl():
-    pass
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>TFL Usage</th>\
+                        </tr>\
+                        "
+    func = ["tfl tube service report",
+            "tfl journey duration from se1 5hp to se18 3px",
+            "tfl live arrivals for 53 at dunton road",
+            "tfl live arrivals for northern at bank underground station"]
+    for i in func:
+        display += f"<tr>\
+                        <td>{i}</td>\
+                    </tr>"
+    say = "Find below How to use the TFL feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_news():
-    pass
+    display = "<table id='t01'>\
+                    <tr>\
+                        <th>News Usage</th>\
+                    </tr>\
+                            "
+    display += f"<tr>\
+                      <td>BBC News</td>\
+                </tr>"
+    say = "Find below How to use the News feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_email():
-    pass
+    display = "<table id='t01'>\
+                    <tr>\
+                        <th>Email Usage</th>\
+                    </tr>\
+                            "
+    display += f"<tr>\
+                      <td>send email</td>\
+                </tr>"
+    say = "Find below How to use the Email feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_google():
-    pass
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>Google Usage</th>\
+                        </tr>\
+                                "
+    display += f"<tr>\
+                    <td>google <what_to_google></td>\
+                </tr>"
+    say = "Find below How to use the google feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_skype():
-    pass
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>Skype Usage</th>\
+                        </tr>\
+                        "
+    func = ["skype chat <friend_name> <message>",
+            "show picture <friend_name>",
+            "birthday for <friend_name>",
+            "skype get last message to <friend_name>"]
+    for i in func:
+        display += f"<tr>\
+                        <td>{i}</td>\
+                    </tr>"
+    say = "Find below How to use the Skype feature"
+    reply = {'display': display, 'say': say}
+    return reply
+
 
 def man_wiki():
-    pass
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>Wikipedia Usage</th>\
+                        </tr>\
+                                "
+    display += f"<tr>\
+                    <td>what is <what_to_look_up></td>\
+                </tr>"
+    say = "Find below How to use the Wikipedia feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_facebook():
-    pass
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>Facebook Usage</th>\
+                        </tr>\
+                        "
+    func = ["show my facebook posts",
+            "show facebook pages i like",
+            "how many facebook friends do i have"]
+    for i in func:
+        display += f"<tr>\
+                        <td>{i}</td>\
+                    </tr>"
+    say = "Find below How to use the Facebook feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_football():
-    pass
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>Football Usage</th>\
+                        </tr>\
+                        "
+    func = ["football match today",
+            "football league start",
+            "football league status",
+            "football top scorers",
+            "football top scorers graph",
+            "football match schedules for match 11",
+            ]
+    for i in func:
+        display += f"<tr>\
+                        <td>{i}</td>\
+                    </tr>"
+    say = "Find below How to use the Football feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_time():
-    pass
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>Time Usage</th>\
+                        </tr>\
+                        "
+    func = ("what is the time", "time", "what is the current time", "current time")
+    for i in func:
+        display += f"<tr>\
+                        <td>{i}</td>\
+                    </tr>"
+    say = "Find below How to use the Time feature"
+    reply = {'display': display, 'say': say}
+    return reply
+
+
+def man_date():
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>Date Usage</th>\
+                        </tr>\
+                        "
+    func = ("what is the date", "what is todays date", "todays date", "current date", "date")
+    for i in func:
+        display += f"<tr>\
+                        <td>{i}</td>\
+                    </tr>"
+    say = "Find below How to use the Date feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_weather():
-    pass
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>Weather Usage</th>\
+                        </tr>\
+                        "
+    func = ("weather forecast today", "weather forecast Lagos Nigeria")
+    for i in func:
+        display += f"<tr>\
+                        <td>{i}</td>\
+                    </tr>"
+    say = "Find below How to use the Weather feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_youtube():
-    pass
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>Youtube Usage</th>\
+                        </tr>\
+                                "
+    display += f"<tr>\
+                    <td>play <video_name></td>\
+                </tr>"
+    say = "Find below How to use the Youtube feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_amazon():
-    pass
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>Amazon Usage</th>\
+                        </tr>\
+                        "
+    func = ["amazon least price for <item_name>",
+            "amazon max price for <item_name>",
+            "amazon sort price for speakers at 11",
+            "amazon sort rating for speakers at 4.5"
+            ]
+    for i in func:
+        display += f"<tr>\
+                        <td>{i}</td>\
+                    </tr>"
+    say = "Find below How to use the Amazon feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_dictionary():
-    pass
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>Dictionary Usage</th>\
+                        </tr>\
+                        "
+    func = ["dictionary definition for <word>",
+            "dictionary synonym for <word>",
+            "dictionary antonym for <word>",
+            "dictionary translate <sentence> to <language>"
+            ]
+    for i in func:
+        display += f"<tr>\
+                        <td>{i}</td>\
+                    </tr>"
+    say = "Find below How to use the Dictionary feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_iot():
-    pass
+    display = "<table id='t01'>\
+                    <tr>\
+                        <th>IoT Usage</th>\
+                    </tr>\
+                    "
+    func = ["iot graph from <ip_address>",
+            "iot light off for <ip_address>",
+            "iot light on for <ip_address>",
+            "iot temperature for <ip_address>",
+            "iot cpu for <ip_address>",
+            "iot memory for <ip_address>",
+            "iot humidity for <ip_address>"]
+    for i in func:
+        display += f"<tr>\
+                        <td>{i}</td>\
+                    </tr>"
+    say = "Find below How to use the I.o.T feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 def man_word_cloud():
-    pass
+    display = "<table id='t01'>\
+                        <tr>\
+                            <th>Word Cloud Usage</th>\
+                        </tr>\
+                        "
+    func = ["word cloud <word>",
+            "word cloud antonyms and synonyms <word>",
+            "word cloud twitter <word>"
+            ]
+    for i in func:
+        display += f"<tr>\
+                        <td>{i}</td>\
+                    </tr>"
+    say = "Find below How to use the Word Cloud feature"
+    reply = {'display': display, 'say': say}
+    return reply
 
 
 man_dict = {'man help': man_help, 'man maths': man_maths, 'man twitter': man_twitter, 'man tfl': man_tfl,
             'man news': man_news, 'man email': man_email, 'man skype': man_skype, 'man facebook': man_facebook,
-            'man football': man_football, 'man time': man_time, 'man date': man_time, 'man weather': man_weather,
+            'man football': man_football, 'man time': man_time, 'man date': man_date, 'man weather': man_weather,
             'man youtube': man_youtube, 'man google': man_google, 'man wikipedia': man_wiki, 'man wiki': man_wiki,
             'man amazon': man_amazon, 'man dictionary': man_dictionary, 'man iot': man_iot, 'man word cloud': man_word_cloud}
 
