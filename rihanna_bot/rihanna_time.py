d8b09d02adbad7b59431a63c27b859c653f2a936
Emeka Ugwuanyi <emylincon@gmail.com>
2020-01-27 12:48:41 +0000

    refactored

--- rihanna_bot/rihanna_time.py
+++ rihanna_bot/rihanna_time.py
@@ -6,7 +6,7 @@ import config
 
 def rihanna_time():
     _time = str(datetime.datetime.now()).split()[1].split('.')[0]
-    reply =  f'The Time is {_time}'
+    reply = f'The Time is {_time}'
     if config.lang_code != 'en':
         reply = trans_.translate_sentence_code(reply, config.lang_code)
         config.lang_code = 'en'
