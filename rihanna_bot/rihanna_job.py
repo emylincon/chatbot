d6d8e0798bda2d1cb2ab0a66a5bdc52948202e61
Emeka Ugwuanyi <emylincon@gmail.com>
2020-01-30 16:59:28 +0000

    added rihanna_job module

---
+++ rihanna_bot/rihanna_job.py
@@ -0,0 +1,88 @@
+import requests
+from bs4 import BeautifulSoup
+import config
+
+
+def selector(message):
+    if message[:len("job search average salary for ")] == "job search average salary for ":
+        query = message[len("job search average salary for "):].split(' in ')
+        job = query[0].strip()
+        place = query[1].strip()
+        return average_salary(job,place)
+
+
+def go_to(url):
+    page = requests.get(url, headers=config.header)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+
+def search_job(job, where):
+    url = f"https://www.indeed.co.uk/jobs?q={job}&l={where}"
+    page = requests.get(url, headers=config.header)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    #items = soup.find_all("div", {"class": "jobsearch-jobDescriptionText"})
+    items = soup.find_all("div", {"class": "jobsearch-SerpJobCard unifiedRow row result"})
+    min_ = []
+    max_ = []
+
+    for job_post in items:
+        try:
+            salary_raw = job_post.find("span", {"class": "salaryText"}).get_text()
+
+            if '-' in salary_raw:
+                salary_raw = salary_raw.split('-')
+                con = salary_raw[1].split()[-1].strip()
+                if con == 'year':
+                    multi = 1
+                elif con == 'month':
+                    multi = 12
+                else:
+                    multi = 5*4*12
+                min_.append(float(salary_raw[0].strip()[1:].replace(',', ''))*multi)
+                max_.append(float(salary_raw[1].split()[0].strip()[1:].replace(',', ''))*multi)
+        except AttributeError:
+            pass
+    
+    return min_, max_
+
+
+def add_format(query):
+    query = str(round(query, 2))
+    raw = query.split('.')
+    points = raw[1] if len(raw[1]) == 2 else raw[1]+'0'
+    int_ = format(int(raw[0]), ',d')
+
+    return f'{int_}.{points}'
+
+
+def average_salary(job, place):
+    min_, max_ = search_job(job,place)
+    if len(min_) == 0:
+        return f"rihanna could not find {job}"
+    else:
+        avg_min = add_format(sum(min_)/len(min_))
+        avg_max = add_format(sum(max_)/len(max_))
+        display = f"The average annual salary for {job} in {place.capitalize()} ranges from £{avg_min} to £{avg_max}"
+        reply = {'display': display, 'say': display}
+        return reply
+
+
+def min_salary(job, place):  # TODO
+    # add min salary and job link
+    pass
+
+
+def max_salary(job, place):  # TODO
+    # add max salary and job link
+    pass
+
+
+def key_skills(job, place):  # TODO
+    # find job key skills
+    pass
+
+
+def salary_graph(job, place):  # TODO
+    # return average salary graph of top cities in uk
+    pass
+
