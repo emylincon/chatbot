041a88219830dadd950e674f78163bf4c68e03b9
Emeka Ugwuanyi <emylincon@gmail.com>
2020-02-07 10:15:18 +0000

    added google images search

--- rihanna.py
+++ rihanna.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 from rihanna_bot import rihanna_football, rihanna_speak, rihanna_tweet, rihanna_news, rihanna_skype, rihanna_one_char, \
     rihanna_time, rihanna_maths as calc, rihanna_email, rihanna_tfl, rihanna_spell, rihanna_facebook, rihanna_amazon, \
     rihanna_dict, rihanna_iot, rihanna_wc, \
-    rihanna_man, rihanna_job, rihanna_youtube
+    rihanna_man, rihanna_job, rihanna_youtube, rihanna_google_image
 import config
 import random as r
 
@@ -178,6 +178,9 @@ def rihanna(message):
 
     elif message[:len('word cloud')] == 'word cloud':
         return rihanna_wc.selector(message)
+
+    elif message[:len('google image')] == 'google image':
+        return rihanna_google_image.selector(message)
 
     elif ("twitter" in message) or ("tweet" in message):
         return rihanna_tweet.twitter(message)
