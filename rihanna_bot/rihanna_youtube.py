1b9c52553626e1b44b479c3c3f1247b2473ea3ca
Emeka Ugwuanyi <emylincon@gmail.com>
2020-02-07 10:37:08 +0000

    refactored file

--- rihanna_bot/rihanna_youtube.py
+++ rihanna_bot/rihanna_youtube.py
@@ -21,12 +21,12 @@ def search_youtube(query):
         req = url + query
         page = requests.get(req, headers=config.header)
         soup = BeautifulSoup(page.content, 'html.parser')
-        load= soup.find("div", {"id": "img-preload"})
+        load = soup.find("div", {"id": "img-preload"})
         li = load.find_all("img")
         vid = li[0].get("src").split('/')[4]
-        #link = "https://www.youtube.com/watch?v=" + vid
+        # link = "https://www.youtube.com/watch?v=" + vid
         display = f'<iframe width="560" height="315"\
-                src="https://www.youtube.com/embed/{vid}?autoplay=1" frameborder="0" allowfullscreen>\
+                src="https://www.youtube.com/embed/{vid}?rel=0" allow="autoplay" frameborder="0" allowfullscreen>\
                 </iframe>'
         say = f"playing {query} video from youtube"
         reply = {'display': display, 'say': say}
@@ -40,10 +40,10 @@ def search_youtube_loop(query):
         req = url + query
         page = requests.get(req, headers=config.header)
         soup = BeautifulSoup(page.content, 'html.parser')
-        load= soup.find("div", {"id": "img-preload"})
+        load = soup.find("div", {"id": "img-preload"})
         li = load.find_all("img")
         vid = li[0].get("src").split('/')[4]
-        #link = "https://www.youtube.com/watch?v=" + vid
+        # link = "https://www.youtube.com/watch?v=" + vid
         display = f'<iframe width="560" height="315"\
                 src="https://www.youtube.com/embed/{vid}?playlist={vid}&loop=1" frameborder="0" allowfullscreen>\
                 </iframe>'
@@ -53,4 +53,4 @@ def search_youtube_loop(query):
     except Exception as e:
         return {'display': e, 'say': e}
 
-#https://www.w3schools.com/html/html_youtube.asp
+# https://www.w3schools.com/html/html_youtube.asp
