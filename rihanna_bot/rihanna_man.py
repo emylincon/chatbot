c6d2f7e22a5c1326a063ecf26cff840b3999eff5
Emeka Ugwuanyi <emylincon@gmail.com>
2020-02-07 10:32:28 +0000

    added google images search

--- rihanna_bot/rihanna_man.py
+++ rihanna_bot/rihanna_man.py
@@ -394,10 +394,33 @@ def man_job_search():
     return reply
 
 
+def man_google_image():
+    display = "<table id='t01'>\
+                            <tr>\
+                                <th>Google Image Usage</th>\
+                                <th>Description</th>\
+                            </tr>\
+                            "
+    func = ["Google image <b>image_query</b>",
+            "Google images <b>image_query</b>",
+            ]
+    des = ["returns a single image to match search query", "returns at least 10 images to match search query"]
+    for i in func:
+        j = i.replace('<b>', '[').replace('</b>', ']')
+        display += f"<tr>\
+                            <td onclick='man_complete("+f'"{j}"'+f")'>{i}</td>\
+                            <td onclick='man_complete(" + f'"{j}"' + f")'>{des[func.index(i)]}</td>\
+                        </tr>"
+    display += "</table>"
+    say = "Find below How to use the google image feature"
+    reply = {'display': display, 'say': say}
+    return reply
+
+
 man_dict = {'man help': man_help, 'man maths': man_maths, 'man twitter': man_twitter, 'man tfl': man_tfl,
             'man news': man_news, 'man email': man_email, 'man skype': man_skype, 'man facebook': man_facebook,
             'man football': man_football, 'man time': man_time, 'man date': man_date, 'man weather': man_weather,
             'man youtube': man_youtube, 'man google': man_google, 'man wikipedia': man_wiki, 'man wiki': man_wiki,
             'man amazon': man_amazon, 'man dictionary': man_dictionary, 'man iot': man_iot,
-            'man word cloud': man_word_cloud, 'man job search': man_job_search}
+            'man word cloud': man_word_cloud, 'man job search': man_job_search, 'man google image': man_google_image}
 
