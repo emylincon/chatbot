8107e40214b197660fe5c080eb088a49c716519a
Emeka Ugwuanyi <emylincon@gmail.com>
2020-02-18 19:41:57 +0000

    improved the docker module

--- rihanna_bot/rihanna_docker.py
+++ rihanna_bot/rihanna_docker.py
@@ -2,13 +2,34 @@ import paramiko
 import config as cf
 import json
 import ast
+import docker
 
 
 def selector(message):
-    if message == 'docker container list':
-        return docker_container()
-    elif message == 'docker image list':
-        return docker_image()
+    if message in docker_dict:
+        return docker_dict[message]()
+    elif message[:len('docker run')] == 'docker run':
+        image = message[len('docker run')+1:].strip()
+        return run_container(image)
+    elif message[:len('docker multi run')] == 'docker multi run':
+        para = message[len('docker multi run')+1:].strip().split()
+        return multi_run(image=para[1], no=para[0])
+    elif message[:len('docker start')] == 'docker start':
+        image = message[len('docker start')+1:].strip()
+        return start_container(image)
+    elif message[:len('docker stop')] == 'docker stop':
+        image = message[len('docker stop')+1:].strip()
+        return stop_container(image)
+    elif message[:len('docker container delete')] == 'docker container delete':
+        image = message[len('docker container delete')+1:].strip()
+        return delete_container(image)
+    elif message[:len('docker image delete')] == 'docker image delete':
+        image = message[len('docker image delete')+1:].strip()
+        return delete_image(image)
+    elif message[:len('docker pull')] == 'docker pull':
+        image = message[len('docker pull')+1:].strip()
+        return delete_container(image)
+
     else:
         return "docker is offline"
 
@@ -58,7 +79,7 @@ def docker_container():
             "<tr>\
             "
     for i in tags:
-        f"<th><font color='blue'>{i}</font></th>"
+        reply += f"<th><font color='blue'>{i}</font></th>"
     reply += "</tr>"
 
     for container in data:
@@ -73,5 +94,173 @@ def docker_container():
     return reply_
 
 
+def run_container(image):
+    do = f"docker run -ti --detach {image}"
+    data, err = send_command(do)
+    if len(data) == 0 and len(err) > 0:
+        return "Error occurred could not find image"
+    else:
+        return "container is running"
+
+
+def multi_run(image, no):
+    try:
+        for i in range(int(no)):
+            run_container(image)
+        return f"{no} {image} has been deployed on server"
+    except Exception as e:
+        return f"Error in multi_run: {e}"
+
+
+def start_container(con_name):
+    do = f"docker start {con_name}"
+    data, err = send_command(do)
+    if len(data) == 0 and len(err) > 0:
+        return "Error occurred could not find image"
+    else:
+        return "container is running"
+
+
+def _stop_container(con_name):
+    do = f"docker stop {con_name}"
+    data, err = send_command(do)
+    if len(data) == 0 and len(err) > 0:
+        return "Error occurred could not find image"
+    else:
+        return "container is running"
+
+
+def stop_container(con_name):
+    client = docker.DockerClient(base_url=cf.docker_host['connect'])
+    try:
+        con = client.containers.get(con_name)
+        con.stop()
+        return f"{con_name} has been stopped"
+    except Exception as e:
+        return f'stop container error: {e}'
+
+
+def prune_containers():
+    client = docker.DockerClient(base_url=cf.docker_host['connect'])
+    client.containers.prune()
+    return "All stopped containers have been deleted"
+
+
+def delete_container(con_name):
+    do = f"docker container rm {con_name}"
+    data, err = send_command(do)
+    if len(data) == 0 and len(err) > 0:
+        return "Error occurred could not find image"
+    else:
+        return f"{con_name} container has been deleted"
+
+
+def delete_image(image):
+    do = f"docker image rm {image}"
+    #print(do)
+    data, err = send_command(do)
+    if len(data) == 0 and len(err) > 0:
+        return "Error occurred could not find image"
+    else:
+        return f"{image} Image has been deleted"
 #docker_container()
 
+
+def docker_container_list():
+    client = docker.DockerClient(base_url=cf.docker_host['connect'])
+    data = client.containers.list(all)
+    tags = ['Id', 'Names', 'Image', 'Status']
+    reply = "<table id='t01'>" \
+            "<tr>\
+            "
+    for i in tags:
+        reply += f"<th><font color='blue'>{i}</font></th>"
+    reply += "</tr>"
+    for container in data:
+        reply += f"<tr>"
+        reply += f"<td>{container.attrs['Config']['Hostname']}</td>"
+        reply += f"<td>{container.attrs['Name'][1:]}</td>"
+        reply += f"<td>{container.attrs['Config']['Image']}</td>"
+        status = container.attrs['State']['Status']
+        if status == 'running':
+            reply += f"<td><font color='green'>{status}</font></td>"
+        elif status == 'created':
+            reply += f"<td><font color='yellow'>{status}</font></td>"
+        else:
+            reply += f"<td><font color='red'>{status}</font></td>"
+        reply += f"<tr>"
+    reply += "</table>"
+    reply_ = {'display': reply, 'say': 'Find below the list of containers'}
+    client.close()
+    return reply_
+
+
+def container_network():
+    client = docker.DockerClient(base_url=cf.docker_host['connect'])
+    data = client.containers.list()
+    tags = ['Id', 'Names', 'Image', 'IPAddress', 'MacAddress', 'Gateway']
+    reply = "<table id='t01'>" \
+            "<tr>\
+            "
+    for i in tags:
+        reply += f"<th><font color='blue'>{i}</font></th>"
+    reply += "</tr>"
+    for container in data:
+        reply += f"<tr>"
+        reply += f"<td>{container.attrs['Config']['Hostname']}</td>"
+        reply += f"<td>{container.attrs['Name'][1:]}</td>"
+        reply += f"<td>{container.attrs['Config']['Image']}</td>"
+        reply += f"<td>{container.attrs['NetworkSettings']['IPAddress']}</td>"
+        reply += f"<td>{container.attrs['NetworkSettings']['MacAddress']}</td>"
+        reply += f"<td>{container.attrs['NetworkSettings']['Gateway']}</td>"
+        reply += f"<tr>"
+    reply += "</table>"
+    reply_ = {'display': reply, 'say': 'Find below the Network configurations of the running containers'}
+    client.close()
+    return reply_
+
+
+def container_utils():
+    client = docker.DockerClient(base_url=cf.docker_host['connect'])
+    data = client.containers.list()
+    tags = ['Id', 'Names', 'Image', 'CPU Usage', 'Memory Usage']
+    reply = "<table id='t01'>" \
+            "<tr>\
+            "
+    for i in tags:
+        reply += f"<th><font color='blue'>{i}</font></th>"
+    reply += "</tr>"
+    for container in data:
+        reply += f"<tr>"
+        reply += f"<td>{container.attrs['Config']['Hostname']}</td>"
+        reply += f"<td>{container.attrs['Name'][1:]}</td>"
+        reply += f"<td>{container.attrs['Config']['Image']}</td>"
+        reply += f"<td>{container.attrs['HostConfig']['CpuPercent']}</td>"
+        reply += f"<td>{container.attrs['HostConfig']['Memory']}</td>"
+        reply += f"<tr>"
+    reply += "</table>"
+    reply_ = {'display': reply, 'say': 'Find below the Utilization of the running containers'}
+    client.close()
+    return reply_
+
+
+def run_container_cmd(image, cmd):
+    client = docker.DockerClient(base_url=cf.docker_host['connect'])
+    con = client.containers.run('ubuntu', command='it', detach=True)
+    # not completing
+
+
+def docker_pull(image):
+    client = docker.DockerClient(base_url=cf.docker_host['connect'])
+    try:
+        client.images.pull(image)
+        client.close()
+        return f"{image} created"
+    except Exception:
+        client.close()
+        return f"could not find {image}"
+
+
+docker_dict = {'docker image list': docker_image, 'docker container list': docker_container_list,
+               'docker container network': container_network, 'docker container utilization': container_utils,
+               'docker prune': prune_containers}
