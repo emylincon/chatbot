71d783c93d638831f04637a14f6c41999a491efb
Emeka Ugwuanyi <emylincon@gmail.com>
2020-03-16 23:05:41 +0000

    added new news feature

--- rihanna.py
+++ rihanna.py
@@ -6,7 +6,7 @@ import pyttsx3
 from selenium import webdriver
 from rihanna_bot import rihanna_football, rihanna_speak, rihanna_tweet, rihanna_news, rihanna_skype, rihanna_one_char, \
     rihanna_time, rihanna_maths as calc, rihanna_email, rihanna_tfl, rihanna_spell, rihanna_facebook, rihanna_amazon, \
-    rihanna_dict, rihanna_iot, rihanna_wc, rihanna_sound_cloud, \
+    rihanna_dict, rihanna_iot, rihanna_wc, rihanna_sound_cloud, ri_news, \
     rihanna_man, rihanna_job, rihanna_youtube, rihanna_google_image, rihanna_windows, rihanna_docker, rihanna_nhs
 import config
 import random as r
@@ -249,6 +249,9 @@ def rihanna(message):
     elif {"bbc", "news"} - set(message.split()) == set():
         reply = rihanna_news.bbc()
         return reply
+
+    elif message[:len('news')] == 'news':
+        return ri_news.selector(message)
 
     elif message == 'weather forecast today':
         reply = weather('london,uk')
