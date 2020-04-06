2b5d4ab17407d312ebfaf78f0197979d85481218
Emeka Ugwuanyi <emylincon@gmail.com>
2020-04-06 20:58:36 +0100

    commit man rihanna

--- rihanna_bot/rihanna_man.py
+++ rihanna_bot/rihanna_man.py
@@ -1,3 +1,174 @@
+select = '<option value="hot-100">HOT 100</option>\
+<option value="billboard-200">BILLBOARD 200</option>\
+<option value="artist-100">ARTIST 100</option>\
+<option value="social-50">SOCIAL 50</option>\
+<option value="streaming-songs">STREAMING SONGS</option>\
+<option value="radio-songs">RADIO SONGS</option>\
+<option value="digital-song-sales">DIGITAL SONG SALES</option>\
+<option value="on-demand-songs">ON DEMAND SONGS</option>\
+<option value="top-album-sales">TOP ALBUM SALES</option>\
+<option value="current-albums">CURRENT ALBUMS</option>\
+<option value="catalog-albums">CATALOG ALBUMS</option>\
+<option value="independent-albums">INDEPENDENT ALBUMS</option>\
+<option value="soundtracks">SOUNDTRACKS</option>\
+<option value="vinyl-albums">VINYL ALBUMS</option>\
+<option value="greatest-billboard-200-albums">GREATEST BILLBOARD 200 ALBUMS</option>\
+<option value="greatest-billboard-200-artists">GREATEST BILLBOARD 200 ARTISTS</option>\
+<option value="greatest-hot-100-singles">GREATEST HOT 100 SINGLES</option>\
+<option value="greatest-hot-100-artists">GREATEST HOT 100 ARTISTS</option>\
+<option value="greatest-hot-100-songs-by-women">GREATEST HOT 100 SONGS BY WOMEN</option>\
+<option value="greatest-hot-100-women-artists">GREATEST HOT 100 WOMEN ARTISTS</option>\
+<option value="greatest-billboard-200-albums-by-women">GREATEST BILLBOARD 200 ALBUMS BY WOMEN</option>\
+<option value="greatest-billboard-200-women-artists">GREATEST BILLBOARD 200 WOMEN ARTISTS</option>\
+<option value="greatest-billboards-top-songs-80s">GREATEST BILLBOARDS TOP SONGS 80S</option>\
+<option value="greatest-billboards-top-songs-90s">GREATEST BILLBOARDS TOP SONGS 90S</option>\
+<option value="greatest-of-all-time-pop-songs">GREATEST OF ALL TIME POP SONGS</option>\
+<option value="greatest-of-all-time-pop-songs-artists">GREATEST OF ALL TIME POP SONGS ARTISTS</option>\
+<option value="greatest-adult-pop-songs">GREATEST ADULT POP SONGS</option>\
+<option value="greatest-adult-pop-artists">GREATEST ADULT POP ARTISTS</option>\
+<option value="greatest-country-songs">GREATEST COUNTRY SONGS</option>\
+<option value="greatest-country-albums">GREATEST COUNTRY ALBUMS</option>\
+<option value="greatest-country-artists">GREATEST COUNTRY ARTISTS</option>\
+<option value="greatest-hot-latin-songs">GREATEST HOT LATIN SONGS</option>\
+<option value="greatest-hot-latin-songs-artists">GREATEST HOT LATIN SONGS ARTISTS</option>\
+<option value="greatest-top-dance-club-artists">GREATEST TOP DANCE CLUB ARTISTS</option>\
+<option value="greatest-r-b-hip-hop-songs">GREATEST R B HIP HOP SONGS</option>\
+<option value="greatest-r-b-hip-hop-albums">GREATEST R B HIP HOP ALBUMS</option>\
+<option value="greatest-r-b-hip-hop-artists">GREATEST R B HIP HOP ARTISTS</option>\
+<option value="greatest-alternative-songs">GREATEST ALTERNATIVE SONGS</option>\
+<option value="greatest-alternative-artists">GREATEST ALTERNATIVE ARTISTS</option>\
+<option value="pop-songs">POP SONGS</option>\
+<option value="adult-contemporary">ADULT CONTEMPORARY</option>\
+<option value="adult-pop-songs">ADULT POP SONGS</option>\
+<option value="country-songs">COUNTRY SONGS</option>\
+<option value="country-albums">COUNTRY ALBUMS</option>\
+<option value="country-streaming-songs">COUNTRY STREAMING SONGS</option>\
+<option value="country-airplay">COUNTRY AIRPLAY</option>\
+<option value="country-digital-song-sales">COUNTRY DIGITAL SONG SALES</option>\
+<option value="bluegrass-albums">BLUEGRASS ALBUMS</option>\
+<option value="americana-folk-albums">AMERICANA FOLK ALBUMS</option>\
+<option value="rock-songs">ROCK SONGS</option>\
+<option value="rock-albums">ROCK ALBUMS</option>\
+<option value="rock-streaming-songs">ROCK STREAMING SONGS</option>\
+<option value="rock-airplay">ROCK AIRPLAY</option>\
+<option value="rock-digital-song-sales">ROCK DIGITAL SONG SALES</option>\
+<option value="alternative-songs">ALTERNATIVE SONGS</option>\
+<option value="alternative-albums">ALTERNATIVE ALBUMS</option>\
+<option value="triple-a">TRIPLE A</option>\
+<option value="hot-mainstream-rock-tracks">HOT MAINSTREAM ROCK TRACKS</option>\
+<option value="hard-rock-albums">HARD ROCK ALBUMS</option>\
+<option value="r-b-hip-hop-songs">R B HIP HOP SONGS</option>\
+<option value="r-b-hip-hop-albums">R B HIP HOP ALBUMS</option>\
+<option value="r-and-b-hip-hop-streaming-songs">R AND B HIP HOP STREAMING SONGS</option>\
+<option value="hot-r-and-b-hip-hop-airplay">HOT R AND B HIP HOP AIRPLAY</option>\
+<option value="hot-r-and-b-hip-hop-recurrent-airplay">HOT R AND B HIP HOP RECURRENT AIRPLAY</option>\
+<option value="r-and-b-hip-hop-digital-song-sales">R AND B HIP HOP DIGITAL SONG SALES</option>\
+<option value="r-and-b-songs">R AND B SONGS</option>\
+<option value="r-and-b-albums">R AND B ALBUMS</option>\
+<option value="r-and-b-streaming-songs">R AND B STREAMING SONGS</option>\
+<option value="r-and-b-digital-song-sales">R AND B DIGITAL SONG SALES</option>\
+<option value="rap-song">RAP SONG</option>\
+<option value="rap-albums">RAP ALBUMS</option>\
+<option value="rap-streaming-songs">RAP STREAMING SONGS</option>\
+<option value="hot-rap-tracks">HOT RAP TRACKS</option>\
+<option value="rap-digital-song-sales">RAP DIGITAL SONG SALES</option>\
+<option value="mainstream-r-and-b-hip-hop">MAINSTREAM R AND B HIP HOP</option>\
+<option value="hot-adult-r-and-b-airplay">HOT ADULT R AND B AIRPLAY</option>\
+<option value="rhythmic-40">RHYTHMIC 40</option>\
+<option value="latin-songs">LATIN SONGS</option>\
+<option value="latin-albums">LATIN ALBUMS</option>\
+<option value="latin-streaming-songs">LATIN STREAMING SONGS</option>\
+<option value="latin-airplay">LATIN AIRPLAY</option>\
+<option value="latin-digital-song-sales">LATIN DIGITAL SONG SALES</option>\
+<option value="regional-mexican-albums">REGIONAL MEXICAN ALBUMS</option>\
+<option value="latin-regional-mexican-airplay">LATIN REGIONAL MEXICAN AIRPLAY</option>\
+<option value="latin-pop-albums">LATIN POP ALBUMS</option>\
+<option value="latin-pop-airplay">LATIN POP AIRPLAY</option>\
+<option value="tropical-albums">TROPICAL ALBUMS</option>\
+<option value="latin-tropical-airplay">LATIN TROPICAL AIRPLAY</option>\
+<option value="latin-rhythm-albums">LATIN RHYTHM ALBUMS</option>\
+<option value="latin-rhythm-airplay">LATIN RHYTHM AIRPLAY</option>\
+<option value="dance-electronic-songs">DANCE ELECTRONIC SONGS</option>\
+<option value="dance-electronic-albums">DANCE ELECTRONIC ALBUMS</option>\
+<option value="dance-electronic-streaming-songs">DANCE ELECTRONIC STREAMING SONGS</option>\
+<option value="dance-electronic-digital-song-sales">DANCE ELECTRONIC DIGITAL SONG SALES</option>\
+<option value="hot-dance-airplay">HOT DANCE AIRPLAY</option>\
+<option value="dance-club-play-songs">DANCE CLUB PLAY SONGS</option>\
+<option value="christian-songs">CHRISTIAN SONGS</option>\
+<option value="christian-albums">CHRISTIAN ALBUMS</option>\
+<option value="christian-streaming-songs">CHRISTIAN STREAMING SONGS</option>\
+<option value="christian-airplay">CHRISTIAN AIRPLAY</option>\
+<option value="christian-digital-song-sales">CHRISTIAN DIGITAL SONG SALES</option>\
+<option value="hot-christian-adult-contemporary">HOT CHRISTIAN ADULT CONTEMPORARY</option>\
+<option value="gospel-songs">GOSPEL SONGS</option>\
+<option value="gospel-albums">GOSPEL ALBUMS</option>\
+<option value="gospel-streaming-songs">GOSPEL STREAMING SONGS</option>\
+<option value="gospel-airplay">GOSPEL AIRPLAY</option>\
+<option value="gospel-digital-song-sales">GOSPEL DIGITAL SONG SALES</option>\
+<option value="classical-albums">CLASSICAL ALBUMS</option>\
+<option value="classical-crossover-albums">CLASSICAL CROSSOVER ALBUMS</option>\
+<option value="traditional-classic-albums">TRADITIONAL CLASSIC ALBUMS</option>\
+<option value="jazz-albums">JAZZ ALBUMS</option>\
+<option value="contemporary-jazz">CONTEMPORARY JAZZ</option>\
+<option value="traditional-jazz-albums">TRADITIONAL JAZZ ALBUMS</option>\
+<option value="jazz-songs">JAZZ SONGS</option>\
+<option value="emerging-artists">EMERGING ARTISTS</option>\
+<option value="heatseekers-albums">HEATSEEKERS ALBUMS</option>\
+<option value="lyricfind-global">LYRICFIND GLOBAL</option>\
+<option value="lyricfind-us">LYRICFIND US</option>\
+<option value="next-big-sound-25">NEXT BIG SOUND 25</option>\
+<option value="hot-holiday-songs">HOT HOLIDAY SONGS</option>\
+<option value="holiday-albums">HOLIDAY ALBUMS</option>\
+<option value="holiday-streaming-songs">HOLIDAY STREAMING SONGS</option>\
+<option value="holiday-songs">HOLIDAY SONGS</option>\
+<option value="holiday-season-digital-song-sales">HOLIDAY SEASON DIGITAL SONG SALES</option>\
+<option value="summer-songs">SUMMER SONGS</option>\
+<option value="canadian-hot-100">CANADIAN HOT 100</option>\
+<option value="canadian-albums">CANADIAN ALBUMS</option>\
+<option value="hot-canada-digital-song-sales">HOT CANADA DIGITAL SONG SALES</option>\
+<option value="canada-emerging-artists">CANADA EMERGING ARTISTS</option>\
+<option value="canada-ac">CANADA AC</option>\
+<option value="canada-all-format-airplay">CANADA ALL FORMAT AIRPLAY</option>\
+<option value="canada-chr-top-40">CANADA CHR TOP 40</option>\
+<option value="canada-country">CANADA COUNTRY</option>\
+<option value="canada-hot-ac">CANADA HOT AC</option>\
+<option value="canada-rock">CANADA ROCK</option>\
+<option value="mexico">MEXICO</option>\
+<option value="mexico-ingles">MEXICO INGLES</option>\
+<option value="mexico-popular">MEXICO POPULAR</option>\
+<option value="mexico-espanol">MEXICO ESPANOL</option>\
+<option value="japan-hot-100">JAPAN HOT 100</option>\
+<option value="k-pop-hot-100">K POP HOT 100</option>\
+<option value="billboard-china-social-chart">BILLBOARD CHINA SOCIAL CHART</option>\
+<option value="billboard-argentina-hot-100">BILLBOARD ARGENTINA HOT 100</option>\
+<option value="official-uk-songs">OFFICIAL UK SONGS</option>\
+<option value="official-uk-albums">OFFICIAL UK ALBUMS</option>\
+<option value="uk-digital-song-sales">UK DIGITAL SONG SALES</option>\
+<option value="euro-digital-song-sales">EURO DIGITAL SONG SALES</option>\
+<option value="france-digital-song-sales">FRANCE DIGITAL SONG SALES</option>\
+<option value="germany-songs">GERMANY SONGS</option>\
+<option value="german-albums">GERMAN ALBUMS</option>\
+<option value="greece-albums">GREECE ALBUMS</option>\
+<option value="italy-albums">ITALY ALBUMS</option>\
+<option value="italy-digital-song-sales">ITALY DIGITAL SONG SALES</option>\
+<option value="spain-digital-song-sales">SPAIN DIGITAL SONG SALES</option>\
+<option value="switzerland-digital-song-sales">SWITZERLAND DIGITAL SONG SALES</option>\
+<option value="australian-albums">AUSTRALIAN ALBUMS</option>\
+<option value="australia-digital-song-sales">AUSTRALIA DIGITAL SONG SALES</option>\
+<option value="blues-albums">BLUES ALBUMS</option>\
+<option value="bubbling-under-hot-100-singles">BUBBLING UNDER HOT 100 SINGLES</option>\
+<option value="cast-albums">CAST ALBUMS</option>\
+<option value="comedy-albums">COMEDY ALBUMS</option>\
+<option value="compilation-albums">COMPILATION ALBUMS</option>\
+<option value="hot-singles-recurrents">HOT SINGLES RECURRENTS</option>\
+<option value="kids-albums">KIDS ALBUMS</option>\
+<option value="new-age-albums">NEW AGE ALBUMS</option>\
+<option value="reggae-albums">REGGAE ALBUMS</option>\
+<option value="tastemaker-albums">TASTEMAKER ALBUMS</option>\
+<option value="world-albums">WORLD ALBUMS</option>\
+<option value="world-digital-song-sales">WORLD DIGITAL SONG SALES</option>'
+
+
 def selector(query):
     if query in man_dict:
         return man_dict[query]()
@@ -462,14 +633,24 @@ def man_hot100():
                                 <th>Description</th>\
                             </tr>\
                             "
-    func = ["hot 100 chart",'hot 100 billboard playlist'
+    chart_select = f'<select id="mySelect">' \
+                   f'{select}' \
+                   f'</select>'
+    func = ["hot 100 chart", 'hot 100 billboard playlist', ['most popular ', chart_select]
             ]
-    des = ["displays chart", 'hot 100 billboard playlist videos from youtube']
+    des = ["displays chart", 'hot 100 billboard playlist videos from youtube', 'most popular']
     for i in func:
-        display += f"<tr>\
-                            <td onclick='man_complete(" + f'"{i}"' + f")'>{i}</td>\
-                            <td onclick='man_complete(" + f'"{i}"' + f")'>{des[func.index(i)]}</td>\
-                        </tr>"
+        if type(i).__name__ == 'list':
+
+            display += f"<tr>\
+                            <td onclick='man_mySelect(" + f'"{i[0]}"' + f")'>{i[0]+i[1]}</td>\
+                            <td onclick='man_mySelect(" + f'"{i[0]}"' + f")'>{des[func.index(i)]}</td>\
+                            </tr>"
+        else:
+            display += f"<tr>\
+                                <td onclick='man_complete(" + f'"{i}"' + f")'>{i}</td>\
+                                <td onclick='man_complete(" + f'"{i}"' + f")'>{des[func.index(i)]}</td>\
+                            </tr>"
     display += "</table>"
     say = "Find hot 100 functionality"
     reply = {'display': display, 'say': say}
