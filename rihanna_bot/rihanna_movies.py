b29411ae545fdf25d5c7b73df9a495c3b5dd71c1
Emeka Ugwuanyi <emylincon@gmail.com>
2020-04-14 13:29:11 +0100

    added find_person

--- rihanna_bot/rihanna_movies.py
+++ rihanna_bot/rihanna_movies.py
@@ -20,6 +20,9 @@ def selector(query):
             return Movies().top_movies(end=int(msg), order='year')
         else:
             return Movies().top_movies(order='year')
+    elif query[:len('find person ')] == 'find person ':      # find person angelina in movies
+        msg = query[len('find person '):].split(' in ')[0]
+        return Movies().find_person(msg)
     elif query[:len('find people ')] == 'find people ':      # find people angelina in movies
         msg = query[len('find people '):].split(' in ')[0]
         return Movies().find_people(msg)
@@ -99,7 +102,7 @@ class Movies:
         display = f'<h1 style="text-align:center;">Result for {query}</h1>'
         display += '<table style="width:800px;" border="0">'
         style = "style='font-size:12px; color:#5985E9; font-family:verdana; "
-        p_style = "style='font-size:12px; color:#5985E9; font-family:verdana; margin:0px; padding:0px;"
+        p_style = "style='font-size:12px; color:#5985E9; font-family:verdana; margin:0; padding:0; margin-bottom:0; margin-top:0;'"
         for person in all_people:
             bio = f'<button type="button" class="collapsible">BIO</button>\
                             <div class="content">\
@@ -125,20 +128,129 @@ class Movies:
             except KeyError:
                 nick_name = '-'
             display += f"<tr>\
-                        <td><div {style} float:left;'><img src='{person['headshot']}'><br></div>" \
-                       f"<div {p_style} float:left;'><b>Birth Name</b>: {birth_name}" \
-                       f"<br><b>Known Name</b>:{person['name']}" \
-                       f"<br><b>Height</b>: {height}" \
-                       f"<br><b>BirthDay</b>: {birth_date}" \
-                       f"<br><b>BirthPlace</b>: {birth_place}" \
-                       f"<br><b>Other Names</b>: {nick_name}" \
+                        <td><div style='float:left; margin:0; padding:0; margin-bottom:0; margin-top:0;'><img src='{person['headshot']}'><br></div>" \
+                       f"<div style='float:left; margin:0; padding:0; margin-bottom:0; margin-top:0;'><p {p_style}><b>Birth Name</b>: {birth_name}</p>" \
+                       f"<p {p_style}><b>Known Name</b>:{person['name']}</p>" \
+                       f"<p {p_style}><b>Height</b>: {height}</p>" \
+                       f"<p {p_style}><b>BirthDay</b>: {birth_date}</p>" \
+                       f"<p {p_style}>BirthPlace: {birth_place}</p>" \
+                       f"<p {p_style}><b>Other Names</b>: {nick_name}</p>" \
                        f"</div></td>\
                         </tr>" \
                        f"<tr>" \
                        f"<td>{bio}</td>" \
                        f"</tr>" \
                        f"<tr>" \
-                       f"<td><h3>Filmography</h3>"
+                       f"<td><b>Filmography</b>"
+            for item in person['filmography']:
+                display += f'<button type="button" class="collapsible">{item.capitalize()}</button>'
+                display += ' <div class="content">'
+                sub_table = "<table id='t01'><tr>"
+                for t in person['filmography'][item][0].data:
+                    sub_table += f'<th>{t.capitalize()}</th>'
+                sub_table += '</tr>'
+                for sub_item in person['filmography'][item]:
+                    sub_table += '<tr>'
+                    for it in sub_item.data:
+                        sub_table += f"<td>{sub_item.data[it]}</td>"
+                    sub_table += '</tr>'
+                sub_table += '</table></div>'
+                display += sub_table
+            display += '</td></tr>'
+            try:
+                display += '<tr><td>'
+                display += f'<button type="button" class="collapsible">In Development</button>'
+                display += ' <div class="content">'
+                s_table = "<table id='t01'><tr>"
+                for item in person['in development'][0].data:
+                    s_table += f'<th>{item.capitalize()}</th>'
+                s_table += '</tr>'
+                for item in person['in development']:
+                    s_table += '<tr>'
+                    for it in item.data:
+                        s_table += f"<td>{item.data[it]}</td>"
+                    s_table += '</tr>'
+                s_table += '</table></div>'
+                display += s_table
+                display += '</td></tr>'
+            except KeyError:
+                display += ''
+            try:
+                display += '<tr><td>'
+                display += f'<button type="button" class="collapsible">Salary History</button>'
+                display += ' <div class="content">'
+                for salary in person['salary history']:
+                    sa = salary.split('(qv)::')
+                    display += f'<br><b>{sa[0].replace("_", " ")}</b>: {sa[1]}'
+                display += '</div>'
+                display += '</td></tr>'
+            except KeyError:
+                display += ''
+        display += '</table>'
+
+        script = '<script>\
+                    var coll = document.getElementsByClassName("collapsible");\
+                    var i;\
+                    for (i = 0; i < coll.length; i++) {\
+                      coll[i].addEventListener("click", function() {\
+                        this.classList.toggle("active");\
+                        var content = this.nextElementSibling;\
+                        if (content.style.maxHeight){\
+                          content.style.maxHeight = null;\
+                        } else {\
+                          content.style.maxHeight = content.scrollHeight + "px";\
+                        } \
+                      });\
+                    }\
+                    </script>'
+        display += script
+        say = f'find displayed the results for {query}'
+        return {'display': display, 'say': say}
+
+    def find_person(self, query):
+        people = self.movie.search_person(query)[:1]
+        all_people = [self.movie.get_person(p.personID).data for p in people]
+        display = '<table style="width:800px;" border="0">'
+        p_style = "style='font-size:13px; font-family:verdana; margin:0; padding:0; margin-bottom:0; margin-top:0;'"
+        for person in all_people:
+            bio = f'<button type="button" class="collapsible">BIO</button>\
+                            <div class="content">\
+                             {" ".join(person["mini biography"])} </div>'
+            try:
+                birth_place = person['birth info']['birth place']
+            except KeyError:
+                birth_place = '-'
+            try:
+                birth_name = person['birth name']
+            except KeyError:
+                birth_name = person['name']
+            try:
+                height = person['height']
+            except KeyError:
+                height = '-'
+            try:
+                birth_date = person['birth date']
+            except KeyError:
+                birth_date = '-'
+            try:
+                nick_name = ' '.join(person['akas'])
+            except KeyError:
+                nick_name = '-'
+            display += f"<tr>\
+                        <td><div style='float:left; margin:0; padding:0; margin-bottom:0; margin-top:0;'><img src='{person['headshot']}'><br></div>" \
+                       f"<div style='float:left; margin:0; padding:0; margin-bottom:0; margin-top:0;'><p {p_style}><b>Birth Name</b>: {birth_name}</p>" \
+                       f"<p {p_style}><b>Known Name</b>:{person['name']}</p>" \
+                       f"<p {p_style}><b>Height</b>: {height}</p>" \
+                       f"<p {p_style}><b>BirthDay</b>: {birth_date}</p>" \
+                       f"<p {p_style}><b>BirthPlace</b>: {birth_place}</p>" \
+                       f"<p {p_style}><b>Other Names</b>: {nick_name}</p>" \
+                       f"</div></td>\
+                        </tr>" \
+                       f"<tr>" \
+                       f"<td>{bio}</td>" \
+                       f"</tr>" \
+                       f"<tr>" \
+                       f"<td><b>Filmography</b>"
             for item in person['filmography']:
                 display += f'<button type="button" class="collapsible">{item.capitalize()}</button>'
                 display += ' <div class="content">'
