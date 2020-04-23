e5c49e1c18c8f2657f9eb28d03b20ffd77b926cc
Emeka Ugwuanyi <emylincon@gmail.com>
2020-04-23 21:22:33 +0100

    added mouse event functions

--- javascript/3script.js
+++ javascript/3script.js
@@ -172,3 +172,26 @@ var ws = new WebSocket("ws://localhost:8000");
       document.getElementById("chat_input").value = sen+sel;
 
     }
+    function mouseFunction(e){
+  		var body = document.querySelector('body');
+  		var heart = document.createElement("span");
+  		var x = e.offsetX;
+  		var y = e.offsetY;
+  		heart.style.left = x+'px';
+  		heart.style.top = y+'px';
+  		var size = Math.random() * 100;
+  		heart.style.width = 20 + size+'px';
+  		heart.style.height = 20 + size+'px';
+  		body.appendChild(heart);
+
+      setTimeout(function(){
+        heart.remove();
+      },2000)
+  	}
+
+  	function putMouse(){
+  	document.addEventListener("mousemove", mouseFunction);
+  	}
+  	function removeMouse(){
+	document.removeEventListener("mousemove", mouseFunction);
+	}
