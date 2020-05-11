b2cf0c51459627a736e34d7cf5c3bada8758295e
Emeka Ugwuanyi <emylincon@gmail.com>
2020-05-11 19:50:27 +0100

    rihanna_covid

--- rihanna_bot/rihanna_covid.py
+++ rihanna_bot/rihanna_covid.py
@@ -1,10 +1,13 @@
-import requests
+import datetime
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
 def selector(query):
-    if query == 'covid cases':
+    if query[:len('covid cases for ')] == 'covid cases for ':
+        msg = query[len('covid cases for '):]
+        return Report().display_place(msg)
+    elif query == 'covid cases':
         return Report().display_all()
 
 
@@ -36,7 +39,7 @@ class Report:
         data = {}
         for tr in tbody:
             row = tr.find_all('td')
-            key = row[0].get_text().replace('\n', '')
+            key = row[0].get_text().replace('\n', '').lower()
             data[key] = {}
             for i in range(len(heading[1:])):
                 data[key][heading[i+1]] = row[i+1].get_text()
@@ -46,20 +49,41 @@ class Report:
     def display_all(self):
         display = "<table id='t01'><tr>"
         for head in self.heading:
-            display += f"<th><p style='font-size:15px; color:#5985E9; font-family:verdana;'>{head}</p></th>"
+            if '/' in head:
+                h = head.split('/')
+                head = f"{h[0]}<br>({h[1]})"
+            display += f"<th><p style='font-size:13px; color:#5985E9; font-family:verdana;'>{head}</p></th>"
         display += "</tr>"
         for country in self.data:
             display += '<tr>'
-            display += f"<td><p style='font-size:12px; color:#3C6BD3; font-family:verdana;'>{country}</p></td>"
+            display += f"<td><p style='font-size:10px; color:#3C6BD3; font-family:verdana;'>{country.title()}</p></td>"
             for value in self.data[country].values():
                 if value == '':
-                    display += f"<td><p style='font-size:12px; color:#3C6BD3; font-family:verdana;'>-</p></td>"
+                    display += f"<td><p style='font-size:10px; color:#3C6BD3; font-family:verdana;'>-</p></td>"
                 else:
-                    display += f"<td><p style='font-size:12px; color:#3C6BD3; font-family:verdana;'>{value}</p></td>"
+                    display += f"<td><p style='font-size:10px; color:#3C6BD3; font-family:verdana;'>{value}</p></td>"
             display += '</tr>'
         display += '</table>'
-        say = 'Find displayed the available data on COVID19'
+        say = f'Find displayed the available data on COVID19 updated on {str(datetime.datetime.now()).split()[0]}'
         return {'display': display, 'say': say}
 
+    def display_place(self, place):
+        if place in self.data:
+            display = "<table id='t01'><tr>"
+            display += f"<td><b>Place</b></td>"
+            display += f"<td>{place.title()}</td></tr>"
+            for value in self.data[place]:
+                display += f"""<tr>
+                              <td style='font-size:16px;'><b>{value}</b></td>
+                              <td>{self.data[place][value]}</td>
+                              </tr>"""
+            display += '</table>'
+            say = f"find covid19 details for {place} updated on {str(datetime.datetime.now()).split()[0]}"
+            return {'display': display, 'say': say}
+        else:
+            reply = 'cannot find place'
+            return {'display': reply, 'say': reply}
 
-# a = Report().get_data()
+
+# a = Report().display_place('usa')
+# print(a)
