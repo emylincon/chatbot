e051d8bf49854551cbf6915d7f3ac06bf8915162
Emeka Ugwuanyi <emylincon@gmail.com>
2020-05-13 18:10:39 +0100

    git ssh done

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+apt install espeak
+pip3 install simplewebsocketserver
+pip3 install ChatterBot==0.8.4
+pip3 install wikipedia
+pip3 install selenium
+pip3 install pyttsx3
+pip3 install bs4
+pip3 install matplotlib
+pip3 install pyspellchecker==0.5.0
+pip3 install paramiko
+pip3 install docker
+pip3 install googletrans
+pip3 install pydictionary
+pip3 install football-data-api
+pip3 install facebook-sdk
+pip3 install skpy
+pip3 install speechrecognition
+pip3 install python-twitter
+pip3 install pillow
+pip3 install wordcloud
+pip3 install billboard
