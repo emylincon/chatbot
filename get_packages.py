0a98492bfbedc8c75fa9d7b5ef0a208d14a32397
Emeka Ugwuanyi <emylincon@gmail.com>
2020-05-14 14:14:03 +0100

    added error management exceptions

---
+++ get_packages.py
@@ -0,0 +1,7 @@
+import subprocess as sp
+
+cmd = ['pip freeze']
+packages = str(sp.check_output(cmd, shell=True), 'utf-8')[0:-1]
+file = open('packages.txt', 'w')
+file.write(packages)
+file.close()
