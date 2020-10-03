bc4afb6c734a6e72e9c5141ac51f1ccb7bebeef5
Emeka Ugwuanyi <emylincon@gmail.com>
2020-10-03 09:35:08 +0100

    simon says functionality

--- rihanna_bot/rihanna_man.py
+++ rihanna_bot/rihanna_man.py
@@ -1044,6 +1044,28 @@ def man_recommend():
     return reply
 
 
+def man_simon_says():
+    display = "<table id='t01'>\
+                                <tr>\
+                                    <th>Simon feature</th>\
+                                    <th>Description</th>\
+                                </tr>\
+                                "
+
+    func = {'simon says <b>Emeka is an amazing person</b>': 'repeats what you say',
+            }
+    for i in func:
+        j = i.replace('<b>', '[').replace('</b>', ']')
+        display += f"<tr>\
+                                 <td onclick='man_complete(" + f'"{j}"' + f")'>{i}</td>\
+                                 <td onclick='man_complete(" + f'"{j}"' + f")'>{func[i]}</td>\
+                             </tr>"
+    display += "</table>"
+    say = "Find below How to use the Simon says functionality"
+    reply = {'display': display, 'say': say}
+    return reply
+
+
 man_dict = {'man help': man_help, 'man maths': man_maths, 'man twitter': man_twitter, 'man tfl': man_tfl,
             'man news': man_news, 'man email': man_email, 'man skype': man_skype, 'man facebook': man_facebook,
             'man football': man_football, 'man time': man_time, 'man date': man_date, 'man weather': man_weather,
@@ -1054,4 +1076,4 @@ man_dict = {'man help': man_help, 'man maths': man_maths, 'man twitter': man_twi
             'man rihanna news': man_news_, 'man rihanna': man_alpha, 'man hot 100': man_hot100, 'man map': man_map,
             'man movies': man_movies, 'man lyrics': man_lyrics, 'man spotify': man_spotify, 'man itunes': man_itunes,
             'man word file': man_wordfile, 'man game': man_games, 'man games': man_games, 'man covid': man_covid,
-            'man recommend': man_recommend}
+            'man recommend': man_recommend, 'man simon says': man_simon_says}
