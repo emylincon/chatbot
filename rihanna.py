ce7a28db0286bfd71c462e590f51e1a9c6c9209a
Emeka Ugwuanyi <emylincon@gmail.com>
2020-10-10 12:27:59 +0100

    rih docker

--- rihanna.py
+++ rihanna.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 from rihanna_bot import rihanna_football, rihanna_speak, rihanna_tweet, rihanna_news, rihanna_skype, rihanna_one_char, \
     rihanna_time, rihanna_maths as calc, rihanna_email, rihanna_tfl, rihanna_spell, rihanna_facebook, rihanna_amazon, \
     rihanna_dict, rihanna_iot, rihanna_wc, rihanna_sound_cloud, ri_news, rihanna_science, rihanna_maps, \
-    rihanna_man, rihanna_job, ri_youtube, ri_image, rihanna_windows, rihanna_docker, rihanna_nhs, hot100, \
+    rihanna_man, rihanna_job, ri_youtube, ri_image, rihanna_windows, ri_docker, rihanna_nhs, hot100, \
     rihanna_movies, rihanna_lyrics, rihanna_spotify, rihanna_itunes, rihanna_docx, rihanna_games, rihanna_covid, \
     rihanna_movieRecommender
 import config
@@ -341,7 +341,7 @@ def rihanna(message):
         elif message.lower()[:len('job search')] == 'job search':
             return rihanna_job.selector(format_string(message).lower().strip())
         elif message.lower()[:len('docker')] == 'docker':
-            return rihanna_docker.selector(message.lower().strip())
+            return ri_docker.selector(message.lower().strip())
         elif message.lower()[:len('game play')] == 'game play':
             return rihanna_games.selector(format_string(message).lower().strip())
         elif message.strip().lower() in laugh:
