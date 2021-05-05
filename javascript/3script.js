c558c82e790aef36e99dd853f5b1ce96df1643a1
emylincon <emylincon@gmail.com>
2021-05-05 07:44:42 +0100

    refactored

--- javascript/3script.js
+++ javascript/3script.js
@@ -186,8 +186,6 @@ var recognition = new window.SpeechRecognition();
       //   msg.volume = 1; // 0 to 1
       //   msg.rate = 1; // 0.1 to 10
       //   msg.pitch = 1.5; // 0 to 2
-      //
-      //
       //   const voice = {
       //   "name":  "Microsoft Hazel Desktop - English (Great Britain)",
       //   "lang": "en-GB"
